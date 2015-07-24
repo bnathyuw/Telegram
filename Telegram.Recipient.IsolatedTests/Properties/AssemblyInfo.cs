5bbf2a8ac7336436e572d935ce2186552625fef2
Matthew Butt <matthew@matthewbutt.com>
2015-07-24 10:43:27 +0100

    Rename UnitTests to IsolatedTests. This is because I am about to put an integration test in that project!

--- Telegram.Recipient.IsolatedTests/Properties/AssemblyInfo.cs
+++ Telegram.Recipient.IsolatedTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Telegram.Recipient.UnitTests")]
+[assembly: AssemblyTitle("Telegram.Recipient.IsolatedTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Telegram.Recipient.UnitTests")]
+[assembly: AssemblyProduct("Telegram.Recipient.IsolatedTests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
