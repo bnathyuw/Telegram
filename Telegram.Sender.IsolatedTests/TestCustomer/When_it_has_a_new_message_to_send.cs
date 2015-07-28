3821f497b71452120abd856e825b62abb8fd28f4
matthew.butt@7digital.com <Matthew Butt>
2015-07-28 15:11:25 +0100

    Rewrite When_it_has_a_new_message_to_send scenario as a step towards asynchrony.

--- Telegram.Sender.IsolatedTests/TestCustomer/When_it_has_a_new_message_to_send.cs
+++ Telegram.Sender.IsolatedTests/TestCustomer/When_it_has_a_new_message_to_send.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Telegram.Sender.IsolatedTests.TestCustomer
@@ -8,29 +9,40 @@ namespace Telegram.Sender.IsolatedTests.TestCustomer
     public class When_it_has_a_new_message_to_send : TextReader
     {
         private Chit? _actualChit;
+        private TextReader _standardIn;
+        private int _messageCounter;
         private const string ExpectedValue = "Hello, world!";
+        private const int NumberOfMessagesToSend = 1;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _actualChit = null;
+            _standardIn = Console.In;
+            _messageCounter = 0;
+            Console.SetIn(this);
+            var customer = new Customer();
+            customer.ChitDelivered += (sender, args) => _actualChit = args.Chit;
+            customer.Start();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            Console.SetIn(_standardIn);
+        }
 
         [Test]
         public void Then_it_delivers_a_chit()
         {
-            _actualChit = null;
-            var standardIn = Console.In;
-            Console.SetIn(this);
-            var customer = new Customer();
-            customer.ChitDelivered += (sender, args) =>
-                {
-                    _actualChit = args.Chit;
-                };
-            customer.Start();
-
             Assert.That(_actualChit, Is.EqualTo(new Chit(ExpectedValue)));
-
-            Console.SetIn(standardIn);
         }
 
         public override string ReadLine()
-         {
-             return ExpectedValue;
-         }
+        {
+            if (_messageCounter++ >= NumberOfMessagesToSend)
+                Thread.Sleep(Timeout.Infinite);
+            return ExpectedValue;
+        }
     }
 }
